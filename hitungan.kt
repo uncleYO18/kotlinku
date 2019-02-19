ab3f506f7863765255f985bbfda11f0f74e174d5
uncleYO18 <dr.agoy007@gmail.com>
2019-02-19 16:18:11 +0700

    upload hitungan

---
+++ hitungan.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+	
+	print("Nilai a = ")
+	val a = readLine()
+
+	print("Nilai b = ")
+	val b = readLine()
+
+	val a2 = Interger.valueOf(a)
+	val b2 = Interger.valueOf(b)
+	
+	val c = a2 +b2
+	println("A adalah $a dan B adalah $b. penjumlahan antar keduanya adalah $c")
+
+}
